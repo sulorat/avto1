975be956f956deacf6760f2b5f0ecc4637afebe6
Дмитрий Серов <dima.bessarab@list.ru>
2023-12-24 17:00:20 +0300

    Добавьте файлы проекта.

---
+++ avto2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace oop{ 
+internal class Program
+{
+        static void Main(string[] args)
+        {
+            avto car = new avto();
+            car.info(car);
+            car.infoOut();
+            car.drive();
+        }
+
+    }
+}
